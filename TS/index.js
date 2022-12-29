cd3280a14887d5683f4407c57bb16482a8a38c16
kimseungdeok <tmdejr1117@gmail.com>
2022-12-29 20:53:37 +0900

    study : 타입스크립트 설치 및 기본적인 문법 학습

---
+++ TS/index.js
@@ -0,0 +1,6 @@
+// 브라우저는 ts를 이해하지 못하기 때문에 ts를 js로 변환해야 한다.
+var myName = "kim";
+// myName = 10; // error
+var numArray = [1, 2, 3];
+var stringArray = ["kim", "lee", "park"];
+var 이름 = { name: "kim" };
---
+++ TS/index.ts
@@ -0,0 +1,8 @@
+// 브라우저는 ts를 이해하지 못하기 때문에 ts를 js로 변환해야 한다.
+let myName :string = "kim";
+// myName = 10; // error
+
+let numArray :number[] = [1,2,3];
+let stringArray :string[] = ["kim", "lee", "park"];
+
+let 이름:{name?:string} = {name:"kim"};
