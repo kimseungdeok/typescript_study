acee5e8d2c2b10c66126c944a741a5bb3f802b76
kimseungdeok <tmdejr1117@gmail.com>
2023-06-26 22:42:46 +0900

    study : 튜플, 객체, 진위값

--- learn-typescript/class-note/1_type-basic.ts
+++ learn-typescript/class-note/1_type-basic.ts
@@ -1,5 +1,27 @@
-let str = 'hello';
+// JS 문자열 선언
+let str = "hello";
+
+// TS 숫자
 let num: number = 10;
-let arr: Array<number> = [1,2,3];
-let heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
-let items: number[] = [1,2,3];
+
+// TS 배열
+let arr: Array<number> = [1, 2, 3];
+let heroes: Array<string> = ["Capt", "Thor", "Hulk"];
+let items: number[] = [1, 2, 3];
+
+// TS 튜플
+let address1: [string, number] = ["gangnam", 100];
+
+// TS 객체
+let obj: object = {};
+let person: object = {
+    name: "capt",
+    age: 100,
+};
+let person2: { name: string, age: number } = {
+    name: "thor",
+    age: 1000,
+};
+
+// TS 진위값
+let show: boolean = true;
