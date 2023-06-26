56d0a5820d9e4a7468437562c41dec033c82f834
kimseungdeok <tmdejr1117@gmail.com>
2023-06-26 20:23:33 +0900

    study : 자바스크립트에 타입이 있을때의 두번 째 장점

---
+++ learn-typescript/why-ts/sample.js
@@ -0,0 +1,8 @@
+function sum(a, b) {
+    return a + b;
+}
+
+sum(10, 20);
+sum(10, '20'); // error
+
+// @ts-check
---
+++ learn-typescript/why-ts/sample.ts
@@ -0,0 +1,8 @@
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+add(1,2)
+
+let result = add(1,2);
+result.toLocaleString();
