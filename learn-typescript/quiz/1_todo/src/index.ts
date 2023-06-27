bc188b2854c1eb5f2a2654b6d60b287ca82f2dd7
kimseungdeok <tmdejr1117@gmail.com>
2023-06-27 20:32:15 +0900

    study : 인터페이스

--- learn-typescript/quiz/1_todo/src/index.ts
+++ learn-typescript/quiz/1_todo/src/index.ts
@@ -1,7 +1,19 @@
-let todoItems: { id: number; title: string; done: boolean }[];
+// type Todo = {
+//     id: number;
+//     title: string;
+//     done: boolean;
+// }
+
+interface Todo {
+    id: number;
+    title: string;
+    done: boolean;
+}
+
+let todoItems: Todo[];
 
 // api
-function fetchTodoItems(): { id: number; title: string; done: boolean }[] {
+function fetchTodoItems(): Todo[] {
     const todos = [
         { id: 1, title: "안녕", done: false },
         { id: 2, title: "타입", done: false },
@@ -11,12 +23,12 @@ function fetchTodoItems(): { id: number; title: string; done: boolean }[] {
 }
 
 // crud methods
-function fetchTodos(): { id: number; title: string; done: boolean }[] {
+function fetchTodos(): Todo[] {
     const todos = fetchTodoItems();
     return todos;
 }
 
-function addTodo(todo: { id: number; title: string; done: boolean }): void {
+function addTodo(todo: Todo): void {
     todoItems.push(todo);
 }
 
@@ -24,28 +36,22 @@ function deleteTodo(index: number): void {
     todoItems.splice(index, 1);
 }
 
-function completeTodo(
-    index: number,
-    todo: { id: number; title: string; done: boolean }
-): void {
+function completeTodo(index: number, todo: Todo): void {
     todo.done = true;
     todoItems.splice(index, 1, todo);
 }
 
 // business logic
-function logFirstTodo(): { id: number; title: string; done: boolean } {
+function logFirstTodo(): Todo {
     return todoItems[0];
 }
 
-function showCompleted(): { id: number; title: string; done: boolean }[] {
+function showCompleted(): Todo[] {
     return todoItems.filter((item) => item.done);
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems(
-    todo1: { id: number; title: string; done: boolean },
-    todo2: { id: number; title: string; done: boolean }
-): void {
+function addTwoTodoItems(todo1: Todo, todo2: Todo): void {
     // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
     addTodo(todo1);
     addTodo(todo2);
