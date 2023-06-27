91d504ab97ff978cbba9ac1ae47bcb7b58571191
kimseungdeok <tmdejr1117@gmail.com>
2023-06-27 20:40:51 +0900

    study : 인터페이스 이론 학습

--- learn-typescript/class-note/3_interface.ts
+++ learn-typescript/class-note/3_interface.ts
@@ -3,7 +3,20 @@ interface Person {
     name: string;
 }
 
+// 변수에 인터페이스 활용
 let seho : Person = {
     age: 33,
     name: '세호'
 }
+
+// 함수에 인터페이스 활용
+function getPerson(person : Person) {
+    console.log(person);
+}
+const seungdeok : Person = {
+    age: 27,
+    name: '승덕'
+}
+
+getPerson(seungdeok);
+
