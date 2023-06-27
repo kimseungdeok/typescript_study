ea3a9f4694df1c55490145ad9d90b08b58a221bc
kimseungdeok <tmdejr1117@gmail.com>
2023-06-27 20:36:50 +0900

    chore : add 함수 주석처리

--- learn-typescript/class-note/2_functions.ts
+++ learn-typescript/class-note/2_functions.ts
@@ -1,7 +1,7 @@
-// 함수의 파라미터에 타입을 정의하는 방식
-function add(a: number, b: number) {
-    return a + b;
-}
+// // 함수의 파라미터에 타입을 정의하는 방식
+// function add(a: number, b: number) {
+//     return a + b;
+// }
 
 // 함수의 반환 값에 타입을 정의하는 방식
 function jsAdd(): number {
