a53427117ccb030f56064330b9225f3e64482cb3
kimseungdeok <tmdejr1117@gmail.com>
2023-06-28 15:49:18 +0900

    study : 제네릭 적용

--- learn-typescript/example/dropdown-generic.ts
+++ learn-typescript/example/dropdown-generic.ts
@@ -1,26 +1,21 @@
-interface Email {
-    value: string;
+interface DropdownItem<T> {
+    value: T;
     selected: boolean;
 }
 
-const emails: Email[] = [
+const emails: DropdownItem<string>[] = [
     { value: "naver.com", selected: true },
     { value: "gmail.com", selected: false },
     { value: "hanmail.net", selected: false },
 ];
 
-interface ProductNumber {
-    value: number;
-    selected: boolean;
-}
-
-const numberOfProducts: ProductNumber[] = [
+const numberOfProducts: DropdownItem<number>[] = [
     { value: 1, selected: true },
     { value: 2, selected: false },
     { value: 3, selected: false },
 ];
 
-function createDropdownItem(item: Email | ProductNumber) {
+function createDropdownItem(item: DropdownItem<string> | DropdownItem<number>) {
     const option = document.createElement("option");
     option.value = item.value.toString();
     option.innerText = item.value.toString();
@@ -29,6 +24,7 @@ function createDropdownItem(item: Email | ProductNumber) {
 }
 
 // NOTE: 이메일 드롭 다운 아이템 추가
+
 emails.forEach(function (email) {
     const item = createDropdownItem(email);
     const selectTag = document.querySelector("#email-dropdown");
